3a493bbb48b7bdd68ce59b7d4fa64840699800b6
Eduardo de Paula Neves <eduardo.paula@gmail.com>
2024-02-05 11:01:24 -0300

    Aula 90

--- mutable.py
+++ mutable.py
@@ -15,3 +15,11 @@ print(id(another_list))
 
 print(shopping_list)
 print(another_list)
+
+a = b = c = d = e = f = another_list
+print(a)
+
+print("Adding cream")
+b.append("cream")
+print(c)
+print(d)
