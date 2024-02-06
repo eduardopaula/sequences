5a04708b57c134ec858cfce8b3a1f6bbe9e72933
Eduardo de Paula Neves <eduardo.paula@gmail.com>
2024-02-06 11:54:24 -0300

    Aula 101

--- buy_computer.py
+++ buy_computer.py
@@ -11,28 +11,24 @@ available_parts = ["computer",
 valid_choices = []
 for i in range(1, len(available_parts) + 1):
     valid_choices.append(str(i))
-print(valid_choices)
 
 current_choise = "-"
 computer_parts = [] # create an empty list
 
 while current_choise != '0':
-    if current_choise in "1234567":
-        print("Adding {}".format(current_choise))
-        if current_choise == '1':
-            computer_parts.append("computer")
-        elif current_choise == '2':
-            computer_parts.append("monitor")
-        elif current_choise == '3':
-            computer_parts.append("keyboard")
-        elif current_choise == '4':
-            computer_parts.append("mouse")
-        elif current_choise == '5':
-            computer_parts.append("mouse mat")
-        elif current_choise == '6':
-            computer_parts.append("hdmi cable")
-        elif current_choise == '7':
-            computer_parts.append("dvd")
+    if current_choise in valid_choices:
+        index = int(current_choise) - 1
+        chosen_part = available_parts[index]
+        print("Index: {}".format(str(index)))
+        print("Chosen part: {}".format(chosen_part))
+        if chosen_part in computer_parts:
+            # it's already in, so remove it
+            print("Removing {}".format(current_choise))
+            computer_parts.remove(chosen_part)
+        else:
+            print("Adding {}".format(current_choise))
+            computer_parts.append(chosen_part)
+        print("Your list now contains: {}".format(computer_parts))
     else:
         print("Please add options from the list below:")
         for number, part in enumerate(available_parts):
