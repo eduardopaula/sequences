fd6e2827c16716cef36a750e1d6caec211e2ca1a
Eduardo de Paula Neves <eduardo.paula@gmail.com>
2024-02-07 10:41:06 -0300

    Aula 114

---
+++ spam.py
@@ -0,0 +1,13 @@
+menu = [
+    ["egg", "bacon"],
+    ["egg", "sausage", "bacon"],
+    ["egg", "spam"],
+    ["egg", "bacon", "spam"],
+    ["egg", "bacon", "sausage", "spam"],
+    ["spam", "bacon", "sausage", "spam"],
+    ["spam", "sausage", "spam", "bacon", "spam", "tomato", "spam"],
+    ["spam", "egg", "spam", "spam", "bacon", "spam"],
+    ["spam", ["spam", ["spam", ["spam"]]]]
+]
+
+print(menu)
