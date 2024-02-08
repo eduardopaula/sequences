408699e9343fa35c748bd989c27a3704f80db47c
Eduardo de Paula Neves <eduardo.paula@gmail.com>
2024-02-08 09:54:27 -0300

    Aula 128

--- tuples_intro.py
+++ tuples_intro.py
@@ -7,6 +7,10 @@ albums = [("Welcome to my Nightmare", "Alice Cooper", 1975),
 
 print(len(albums))
 
+for name, artist, year in albums:
+    print("Album: {}, Artist: {}, Year: {}"
+          .format(name, artist, year))
+
 for album in albums:
     name, artist, year = album
     print("Album: {}, Artist: {}, Year: {}"
